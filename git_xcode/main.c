6ed4084dde01bd71fa70f7efde7f25d99d4de78f
허영준 <neojijon2004@nate.com>
2016-01-05 17:34:50 +0900

    Initial Commit

---
+++ git_xcode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  git_xcode
+//
+//  Created by 허영준 on 2016. 1. 5..
+//  Copyright © 2016년 허영준. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
